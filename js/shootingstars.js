5f7f16f7a961222dab6fcff0927f7b9e52f079b8
emileeapple <mycomputer@Emilees-iMac.local>
2014-12-17 15:14:04 -0800

    added shooting stars.js to js folder

---
+++ js/shootingstars.js
@@ -0,0 +1,25 @@
+function ShootingStars( position, velocity ){
+	this.position = position;
+	this.velocity = velocity.normalize();
+	this.velocity.mult(random(10, 40));
+}
+
+ShootingStars.prototype.show = function(){
+	fill(255);
+	ellipse(this.position.x, this.position.y, 1, 1);
+}
+
+ShootingStars.prototype.update = function(){
+	this.position.add(this.velocity);
+}
+
+ShootingStars.prototype.isDead = function(){
+	// if ((this.position.x < 0 || this.position.x > width) && (this.position.y < 0 || this.position.y > height)){
+	// 	return true;
+	// }else{
+	// 	return false;
+	// }
+
+	return (this.position.x < 0 || this.position.x > width) && (this.position.y < 0 || this.position.y > height/10000000)
+
+}
